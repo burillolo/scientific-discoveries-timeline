f5b30671c8316bb446ad8432573779089c45bd16
Fernando Burillo <fernando.burillo@edreamsodigeo.org>
2025-02-12 22:30:40 +0100

    Fix deploy

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -1,7 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Save } from "@/components/icons/save"
 
-const plane = "plane.png";
 const chat = "chat.svg";
 
 type CardProps = {
